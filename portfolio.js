fc34846c6dba743d10f4759d3d88898d01890e63
ladyami <99187115+ladyami@users.noreply.github.com>
2022-05-24 22:33:08 +0100

    update the whole file
    done working on the navbar

---
+++ portfolio.js
@@ -0,0 +1,25 @@
+let nav = document.querySelector("nav"),
+  toggleBtn = nav.querySelector(".toggle-btn");
+toggleBtn.addEventListener("click", () => {
+  nav.classList.toggle("open");
+});
+
+function onDrag({ movementY }) {
+  let navStyle = window.getComputedStyle(nav),
+    navTop = parseInt(navStyle.top),
+    navHeight = parseInt(navStyle.height),
+    windHeight = window.innerHeight;
+  nav.style.top = navTop > 0 ? `${navTop + movementY}px` : "1px";
+  if (navTop > windHeight - navHeight) {
+    nav.style.top = `${windHeight - navHeight}px`;
+  }
+}
+nav.addEventListener("mousedown", () => {
+  nav.addEventListener("mousemove", onDrag);
+});
+nav.addEventListener("mouseup", () => {
+  nav.addEventListener("mousemove", onDrag);
+});
+nav.addEventListener("mouseleave", () => {
+  nav.addEventListener("mousemove", onDrag);
+});
